655a27545373fd3338fd0ea3d690d07115c6c015
zelgerj <zelger@me.com>
2013-07-10 17:21:19 +0200

    refactored structur for appserver webapp deployment

---
+++ README.md
@@ -0,0 +1,2 @@
+The ApplicationServer Website
+=============================
---
+++ src/WEB-INF/classes/TechDivision/ApplicationServerWebsite/Servlets/SiteServlet.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * TechDivision\ApplicationServerWebsite\Servlets\SiteServlet
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ */
+
+namespace TechDivision\ApplicationServerWebsite\Servlets;
+
+/**
+ * @package     TechDivision
+ * @copyright  	Copyright (c) 2013 <info@techdivision.com> - TechDivision GmbH
+ * @license    	http://opensource.org/licenses/osl-3.0.php
+ *              Open Software License (OSL 3.0)
+ * @author      Johann Zelger <jz@techdivision.com>
+ */
+
+use TechDivision\ServletContainer\Interfaces\Servlet;
+use TechDivision\ServletContainer\Servlets\HttpServlet;
+use TechDivision\ServletContainer\Interfaces\ServletConfig;
+use TechDivision\ServletContainer\Interfaces\ServletRequest;
+use TechDivision\ServletContainer\Interfaces\ServletResponse;
+
+class SiteServlet extends HttpServlet implements Servlet {
+
+    /**
+     * @param ServletRequest $req
+     * @param ServletResponse $res
+     */
+    public function doGet(ServletRequest $req, ServletResponse $res)
+    {
+        /*
+        $connection = Factory::createContextConnection();
+        $session = $connection->createContextSession();
+        $initialContext = $session->createInitialContext();
+
+        $processor = $initialContext->lookup('TechDivision\Slistr\Services\SlistrProcessor');
+        $processor->createSchema();
+        */
+
+        $res->setContent(
+            file_get_contents(__DIR__ . '/../../../../../index.html')
+        );
+     }
+
+}
